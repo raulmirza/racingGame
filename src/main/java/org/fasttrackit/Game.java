2924e647077c485e063d5444077a6a0549e150ec
maraalexandra <mara.alexandra11@yahoo.com>
2020-04-04 14:23:03 +0300

    studied list and enhanced for

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -1,24 +1,56 @@
 package org.fasttrackit;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 public class Game {
 
     private Track[] tracks = new Track[3];
+    private List<Vehicle> competitors = new ArrayList<Vehicle>();
 
     public void start() {
 
 
         System.out.println("Welcome!");
 
-        int playerCountFromUser = getPlayerCountFromUser();
+
 
         initializeTracks();
         displayTracks();
 
+        Track selectedTrack = getSelectedTrackFromUser();
+        System.out.println("Selected track: " + selectedTrack.getName());
+
+        initializeCompetitors();
+
     }
 
+    private void playOneRound(){
+
+        System.out.println("New Round");
+        for ( Vehicle vehicle : competitors){
+            vehicle.accelerate(100);
+        }
+    }
+    private void initializeCompetitors() {
+        int playerCount = getPlayerCountFromUser();
+
+        for (int i = 0; i < playerCount; i++) {
+            System.out.println("Creating vehicle for player " + (i + 1));
+            String name = getVehicleNameFromUser();
+
+            Vehicle vehicle = new Vehicle();
+            vehicle.setName(name);
+            vehicle.setFuelLevel(80);
+            vehicle.setMaxSpeed(260);
+            vehicle.setMileage(ThreadLocalRandom.current().nextDouble(5, 15));
+
+            competitors.add(vehicle);
+        }
+    }
     private void initializeTracks() {
         Track track1 = new Track();
         track1.setName("Imola");
@@ -42,6 +74,13 @@ public class Game {
                 System.out.println((i +1) + " . " + tracks[i].getName() + " - " + tracks[i].getLength());
             }
         }
+    }
+
+    private Track getSelectedTrackFromUser(){
+        System.out.println("Please select a track.");
+        Scanner scanner = new Scanner(System.in);
+        int trackNumber = scanner.nextInt();
+        return tracks[trackNumber - 1];
     }
      private String getVehicleNameFromUser(){
 
